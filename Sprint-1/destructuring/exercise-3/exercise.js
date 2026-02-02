46a5d1141dc0b6cad47f28d9e5a3882b6b65a62b
Laura <laurabejaranoa22@gmail.com>
2026-02-02 21:20:33 +0100

    exercise 3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,17 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let total = 0;
+
+console.log("QTY  ITEM  TOTAL");
+order.forEach((item) => {
+  let { itemName, quantity, unitPricePence } = item;
+  let lineTotal = (quantity * unitPricePence) / 100;
+
+  total += lineTotal;
+
+  console.log(`${quantity} ${itemName.padEnd(20)} ${lineTotal.toFixed(2)}`);
+});
+
+console.log(`\nTotal: ${total.toFixed(2)}`);
